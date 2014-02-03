2328968fe76185e092059715f90a39e8a3145819
Roman Heinrich <roman.heinrich@gmail.com>
2014-02-03 22:23:42 +0100

    first nokogiri test passes!

---
+++ test/test_nokogiri_patches.rb
@@ -0,0 +1,41 @@
+require_relative 'helper'
+require 'pry'
+
+describe 'Nokogiri' do
+  def html_to_nokogiri(html)
+    Nokogiri::HTML.fragment(html)
+  end
+
+  def deindent(source)
+    split_source = source.split("\n")
+    split_source = split_source.map{|x| x.rstrip}
+    split_source = split_source.select{|x| x != ""}
+    min_space    = split_source.min{|x| x.count(" ")}.count(" ")
+    split_source = split_source.map{|x| x[min_space..-1]}.join("\n")
+  end
+
+  it "works" do
+    res      = html_to_nokogiri(%Q{<div class='main'>somecontent</div>})
+    expected = deindent %Q{
+      .main
+        | somecontent
+    }
+    (res.to_slim).must_equal expected
+  end
+
+  it "deindent" do
+    source = %Q{
+      html
+        body
+          div.main
+            | hey but this work fine or not
+    }
+    res = <<SRC
+html
+  body
+    div.main
+      | hey but this work fine or not
+SRC
+    deindent(source).must_equal res.strip
+  end
+end
